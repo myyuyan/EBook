b716f9e3714d9a7380183d79a2bfef47155fe189
myyuyan <34791814+myyuyan@users.noreply.github.com>
2018-07-07 20:19:53 +0800

    Ebook

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QSettings *info = new QSettings(":/info.ini", QSettings::IniFormat);
+    QString ifo=info->value("/info/adress").toString();
+    info->setValue("/info/adress", "192.168.0.1");
+    delete info;
+    QMessageBox::warning(NULL, ifo, ifo, QMessageBox::Yes | QMessageBox::Yes);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include<QMessageBox>
+#include <QSettings>
 namespace Ui {
 class MainWindow;
 }
